1835447905ce4d3ac4e60cb487b974654da6e8b0
Siddharth <sidhu221099@gmail.com>
2018-06-06 14:38:31 +0530

    Create middleway.java

---
+++ Array -1/middleway.java
@@ -0,0 +1,29 @@
+public int sum2(int[] nums) {
+  int sidhu=nums.length; //size of an Array
+  
+  if(sidhu>=2){
+    
+   return  nums[0]+nums[1];   // Add the array value
+   
+   
+}
+
+else if(sidhu==1){
+  
+  
+  return nums[0];
+}
+
+
+  
+  
+  return 0; //if the size of array is zero so return 0
+
+  
+  
+  
+  
+  
+  
+  
+}
